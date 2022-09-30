edbb678e0661f3c272e8aa81f21d46e898a5419a
mdnghtppl <72051060+mdnghtppl@users.noreply.github.com>
2022-09-29 23:39:16 -0400

    a

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let color = document.getElementById("burger");
 toggle.addEventListener('click', function(){
     // console.log("world")
     menu.forEach(col => col.classList.toggle("menu-toggle"))
-    
+    color.style.color = 'black';
     
     if (color.style.color === 'bisque'){
         color.style.color = 'black';
